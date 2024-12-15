15e6a249d79f4cca9eb4390a414bbc59de4c917f
amdRodion <radikgoncharov04@gmail.com>
2024-12-15 22:39:07 +0300

    add new init options

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -26,10 +26,17 @@ INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.id
 FROM users u
          JOIN roles r ON r.name = 'ROLE_ADMIN'
-WHERE u.username = '$2a$10$Sm5kEEbxfE5rRFyKol1TJekO6VSW4.zSKbu4sbdgzGcaMF/9QbRCS';
+WHERE u.username = '$2a$10$Sm5kEEbxfE5rRFyKol1TJekO6VSW4.zSKbu4sbdgzGcaMF/9QbRCS'
+AND NOT EXISTS (SELECT 1
+                FROM user_roles ur
+                WHERE ur.user_id = u.id AND ur.role_id = r.id);
 
 INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.id
 FROM users u
          JOIN roles r ON r.name = 'ROLE_USER'
-WHERE u.username = '$2a$10$OHrY6sYPb/0oMP35ZQcQyu.4WmZQ9hrRcaCfk4uBjkYNhubm58zru';
+WHERE u.username = '$2a$10$OHrY6sYPb/0oMP35ZQcQyu.4WmZQ9hrRcaCfk4uBjkYNhubm58zru'
+AND NOT EXISTS (SELECT 1
+                FROM user_roles ur
+                WHERE ur.user_id = u.id AND ur.role_id = r.id
+                );
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,14 @@ CREATE TABLE IF NOT EXISTS users (
                                      username VARCHAR(255) NOT NULL,
                                      password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS user_roles (
+                                          user_id BIGINT,
+                                          role_id BIGINT,
+                                          PRIMARY KEY (user_id, role_id)
+    );
+
+CREATE TABLE IF NOT EXISTS roles (
+                                     id BIGSERIAL PRIMARY KEY,
+                                     name VARCHAR(255) NOT NULL
+    );

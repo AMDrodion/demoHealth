f67ba2cc2121b87b6c85eb833a59db43ab1c4617
amdRodion <radikgoncharov04@gmail.com>
2024-12-15 22:20:54 +0300

    add init options

---
+++ src/main/resources/data.sql
@@ -0,0 +1,35 @@
+INSERT INTO users (username, password)
+SELECT '$2a$10$Sm5kEEbxfE5rRFyKol1TJekO6VSW4.zSKbu4sbdgzGcaMF/9QbRCS', '$2a$10$Sm5kEEbxfE5rRFyKol1TJekO6VSW4.zSKbu4sbdgzGcaMF/9QbRCS'
+WHERE NOT EXISTS (
+    SELECT 1 FROM users WHERE username = '$2a$10$Sm5kEEbxfE5rRFyKol1TJekO6VSW4.zSKbu4sbdgzGcaMF/9QbRCS'
+);
+
+INSERT INTO users (username, password)
+SELECT '$2a$10$OHrY6sYPb/0oMP35ZQcQyu.4WmZQ9hrRcaCfk4uBjkYNhubm58zru', '$2a$10$OHrY6sYPb/0oMP35ZQcQyu.4WmZQ9hrRcaCfk4uBjkYNhubm58zru'
+WHERE NOT EXISTS (
+    SELECT 1 FROM users WHERE username = '$2a$10$OHrY6sYPb/0oMP35ZQcQyu.4WmZQ9hrRcaCfk4uBjkYNhubm58zru'
+);
+
+INSERT INTO roles (id, name)
+SELECT 1, 'ROLE_ADMIN'
+WHERE NOT EXISTS (
+    SELECT 1 FROM roles WHERE name = 'ROLE_ADMIN'
+);
+
+INSERT INTO roles (id, name)
+SELECT 2, 'ROLE_USER'
+WHERE NOT EXISTS (
+    SELECT 1 FROM roles WHERE name = 'ROLE_USER'
+);
+
+INSERT INTO user_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM users u
+         JOIN roles r ON r.name = 'ROLE_ADMIN'
+WHERE u.username = '$2a$10$Sm5kEEbxfE5rRFyKol1TJekO6VSW4.zSKbu4sbdgzGcaMF/9QbRCS';
+
+INSERT INTO user_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM users u
+         JOIN roles r ON r.name = 'ROLE_USER'
+WHERE u.username = '$2a$10$OHrY6sYPb/0oMP35ZQcQyu.4WmZQ9hrRcaCfk4uBjkYNhubm58zru';
